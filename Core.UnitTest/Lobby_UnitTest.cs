1c86523567627a19db0b26d61e9212720d4f3c87
Micael <micael.belin@gmail.com>
2014-08-17 21:08:23 +0200

    Added unittest project

---
+++ Core.UnitTest/Lobby_UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.UnitTest
+{
+    [TestClass]
+    public class Lobby_UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+
+
+
+    }
+}
