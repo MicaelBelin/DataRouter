dc9df70ecbf095f3248cc3fd58c4354b3cd75a1f
Micael <micael.belin@gmail.com>
2014-08-20 08:32:24 +0200

    Finished with command, request and response management in Abstract connection.

---
+++ Core/TaskExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xintric.DataRouter.Core
+{
+    public static class TaskExtensions
+    {
+        public static void FireAndForget(this Task t)
+        {
+        }
+
+        public static void FireAndForget<T>(this Task<T> t)
+        {
+        }
+    }
+}
